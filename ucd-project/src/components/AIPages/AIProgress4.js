1e3a4c83795a23e33dec44a97806e3ae900a3add
Kisari <seikakukisari@gmail.com>
2023-05-09 16:46:33 +0700

    finalize

--- ucd-project/src/components/AIPages/AIProgress4.js
+++ ucd-project/src/components/AIPages/AIProgress4.js
@@ -7,75 +7,56 @@ import Footer from '../Footer/Footer'
 import AIsubtitle from './AIsubtitle'
 
 const AIProgess4 = () => {
-    const API_KEY = process.env.API_KEY;
+    const API_KEY = "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";
     let { computer, purpose, budget } = useParams();
-    let [prompt, setPrompt] = useState(`You will act as a computer hardware expert. I will ask you for recommendations of PC builds or Laptops with specific purpose of use and budget. If I ask for "PC", you will answer with 2 recommendations for each part of the PC, along with the pros and cons of each item. If I ask for "Laptops", you will answer with 3 recommendations of laptops, along with the pros and cons each item. If budget is not enough, you can simple reply: recStart = {"error": "Insufficient Budget"} = recEnd
+    let [prompt, setPrompt] = useState(`
+    You will act as a computer hardware expert. I will ask you for recommendations of PC builds or Laptops with specific purpose of use and budget. If I ask for "PC", you will answer with 2 recommendations for each part of the PC, along with the pros and cons of each item. If I ask for "Laptops", you will answer with 3 recommendations of laptops, along with the pros and cons each item. If budget is not enough, you can simple reply: recStart = {"error": "Insufficient Budget"} = recEnd
     Your answer will be in the following format for laptops:
     recStart = {"option1": {"name": "some computer name", "pros": "some pros", "cons": "some cons" }, "option2": {"name": "some computer name", "pros": "some pros", "cons": "some cons" }, "option3": {"name": "some computer name", "pros": "some pros", "cons": "some cons" }} = recEnd
     For PC builds, your answer will be in the following format. Remember, isOptional is when the part of the PC is really optional, things like CPU, RAM, Storage,... are mandatory:
     recStart = {"CPU": {"option1": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "option2": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "isOptional": false }, "GPU": {"option1": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "option2": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "isOptional": false }, "RAM": {"option1": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "option2": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "isOptional": false }, "Storage": {"option1": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "option2": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "isOptional": false }, "PowerSupply": {"option1": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "option2": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "isOptional": false }, "Case": {"option1": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "option2": {"name": "name of part", "pros": "some pros", "cons": "some cons"}, "isOptional": false }} = recEnd
     REMEMBER: YOU MUST ALWAYS FOLLOW THE ABOVE FORMAT AND GET STRAIGHT TO IT. NO FURTHER RESPONSES FROM YOU SHOULD BE GENERATED.
     Now my input will be like this: input={computer: ${computer}, purpose: ${purpose}, budget: ${budget}}`);
-    let [isLoading, setIsLoading] = useState(false);
+    let [isLoading, setIsLoading] = useState(true);
     let [result, setResult] = useState("");
-    const resultRef = useRef();
 
-    const sampleError = `recStart = {"error": "Insufficient Budget"} = recEnd`
-    const sampleResult = `recStart = {"CPU": {"option1": {"name": "AMD Ryzen 3 3100", "pros": "Quad-core, good performance for its price", "cons": "May bottleneck high-end GPUs" }, "option2": {"name": "Intel Core i3-10100", "pros": "Quad-core, good single-core performance", "cons": "No integrated graphics"}}, "GPU": {"option1": {"name": "NVIDIA GeForce GT 1030", "pros": "Affordable, low power consumption", "cons": "Limited VRAM, not suitable for demanding tasks"}, "option2": {"name": "AMD Radeon RX 550", "pros": "Affordable, decent performance for its price", "cons": "May struggle with demanding tasks"}}, "RAM": {"option1": {"name": "Crucial Ballistix 8GB DDR4 3200MHz", "pros": "Affordable, good performance for its price", "cons": "Only 8GB of RAM"}, "option2": {"name": "Corsair Vengeance LPX 16GB DDR4 3000MHz", "pros": "16GB of RAM, good performance", "cons": "More expensive than the other option"}}, "Storage": {"option1": {"name": "Kingston A400 240GB SSD", "pros": "Affordable, good performance for its price", "cons": "Limited storage capacity"}, "option2": {"name": "Crucial MX500 500GB SSD", "pros": "More storage capacity, good performance", "cons": "More expensive than the other option"}}, "PowerSupply": {"option1": {"name": "EVGA 400W", "pros": "Affordable, good for low power systems", "cons": "Not suitable for high power systems"}, "option2": {"name": "Corsair CX450M 450W", "pros": "More power, semi-modular", "cons": "More expensive than the other option"}}, "Case": {"option1": {"name": "Cooler Master MasterBox Q300L", "pros": "Affordable, good airflow", "cons": "Limited cable management options"}, "option2": {"name": "Phanteks Eclipse P300A", "pros": "Good cable management, good airflow", "cons": "More expensive than the other option"}}} = recEnd`
-    const sampleResultLaptop = `recStart = {"option1": {"name": "Acer Aspire 5 A515-56-73AP", "pros": "Affordable price, Intel Core i7-1165G7, 15.6-inch FHD IPS display, 8GB DDR4 RAM, 512GB NVMe SSD", "cons": "No discrete graphics card, limited upgradability options"} , "option2": {"name": "ASUS VivoBook 15 F515 Thin and Light Laptop", "pros": "Affordable price, AMD Ryzen 7 5700U, 15.6-inch FHD display, 8GB DDR4 RAM, 512GB NVMe SSD, Integrated Radeon Graphics", "cons": "No discrete graphics card, limited upgradability options"} , "option3": {"name": "Lenovo Ideapad 3 15.6-inch Laptop", "pros": "Affordable price, Intel Core i5-1135G7, 15.6-inch FHD display, 8GB DDR4 RAM, 256GB SSD, Integrated Intel Iris Xe Graphics", "cons": "No discrete graphics card, limited upgradability options"}} = recEnd`
-    const finalResult = (computer === "PC") ? JSON.parse(sampleResult.slice(11, sampleResult.length - 9).replace(/&quot;/ig, '"')) : (computer === "Laptops") ? JSON.parse(sampleResultLaptop.slice(11, sampleResultLaptop.length - 9).replace(/&quot;/ig, '"')) : JSON.parse(sampleError.slice(11, sampleError.length - 9).replace(/&quot;/ig, '"'));
-    console.log(finalResult);
+    // const sampleError = `recStart = {"error": "Insufficient Budget"} = recEnd`
+    // const result = `recStart = {"CPU": {"option1": {"name": "AMD Ryzen 3 3100", "pros": "Quad-core, good performance for its price", "cons": "May bottleneck high-end GPUs" }, "option2": {"name": "Intel Core i3-10100", "pros": "Quad-core, good single-core performance", "cons": "No integrated graphics"}}, "GPU": {"option1": {"name": "NVIDIA GeForce GT 1030", "pros": "Affordable, low power consumption", "cons": "Limited VRAM, not suitable for demanding tasks"}, "option2": {"name": "AMD Radeon RX 550", "pros": "Affordable, decent performance for its price", "cons": "May struggle with demanding tasks"}}, "RAM": {"option1": {"name": "Crucial Ballistix 8GB DDR4 3200MHz", "pros": "Affordable, good performance for its price", "cons": "Only 8GB of RAM"}, "option2": {"name": "Corsair Vengeance LPX 16GB DDR4 3000MHz", "pros": "16GB of RAM, good performance", "cons": "More expensive than the other option"}}, "Storage": {"option1": {"name": "Kingston A400 240GB SSD", "pros": "Affordable, good performance for its price", "cons": "Limited storage capacity"}, "option2": {"name": "Crucial MX500 500GB SSD", "pros": "More storage capacity, good performance", "cons": "More expensive than the other option"}}, "PowerSupply": {"option1": {"name": "EVGA 400W", "pros": "Affordable, good for low power systems", "cons": "Not suitable for high power systems"}, "option2": {"name": "Corsair CX450M 450W", "pros": "More power, semi-modular", "cons": "More expensive than the other option"}}, "Case": {"option1": {"name": "Cooler Master MasterBox Q300L", "pros": "Affordable, good airflow", "cons": "Limited cable management options"}, "option2": {"name": "Phanteks Eclipse P300A", "pros": "Good cable management, good airflow", "cons": "More expensive than the other option"}}} = recEnd`
+    // const sampleResultLaptop = `recStart = {"option1": {"name": "Acer Aspire 5 A515-56-73AP", "pros": "Affordable price, Intel Core i7-1165G7, 15.6-inch FHD IPS display, 8GB DDR4 RAM, 512GB NVMe SSD", "cons": "No discrete graphics card, limited upgradability options"} , "option2": {"name": "ASUS VivoBook 15 F515 Thin and Light Laptop", "pros": "Affordable price, AMD Ryzen 7 5700U, 15.6-inch FHD display, 8GB DDR4 RAM, 512GB NVMe SSD, Integrated Radeon Graphics", "cons": "No discrete graphics card, limited upgradability options"} , "option3": {"name": "Lenovo Ideapad 3 15.6-inch Laptop", "pros": "Affordable price, Intel Core i5-1135G7, 15.6-inch FHD display, 8GB DDR4 RAM, 256GB SSD, Integrated Intel Iris Xe Graphics", "cons": "No discrete graphics card, limited upgradability options"}} = recEnd`
+    // finalResult = (computer === "PC") ? JSON.parse(result.slice(11, result.length - 9).replace(/&quot;/ig, '"')) : (computer === "Laptops") ? JSON.parse(result.slice(11, result.length - 9).replace(/&quot;/ig, '"')) : JSON.parse(result.slice(11, result.length - 9).replace(/&quot;/ig, '"'));
 
-    useEffect(() => {
-        resultRef.current = result;
-    }, [result]);
-
+    let finalResult = result ? JSON.parse(result.slice(11, result.length - 9).replace(/&quot;/ig, '"')) : "";
 
     let handleSubmitPromptBtnClicked = async () => {
         if (prompt !== ``) {
             setIsLoading(true);
             setResult("");
-            let url = "https://api.openai.com/v1/completions";
-            let data = {
-                model: "text-davinci-003",
-                prompt: prompt,
-                temperature: 0.75,
-                top_p: 0.95,
-                max_tokens: 2000,
-                stream: true,
-                n: 1,
-            };
+            let url = "https://api.openai.com/v1/chat/completions";
+            let data =
+            {
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: prompt }]
+            }
+            const getData = async () => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${API_KEY}`
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    const json = await response.json()
+                    setResult(json?.choices[0].message.content);
 
-            let source = new SSE(url, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${API_KEY}`,
-                },
-                method: "POST",
-                payload: JSON.stringify(data),
-            });
-
-            source.addEventListener("message", async (e) => {
-                if (e.data !== "[DONE]") {
-                    let payload = JSON.parse(e.data);
-                    let text = payload.choices[0].text;
-                    if (text !== "\n") {
-                        resultRef.current = resultRef.current + text;
-                        setResult(resultRef.current);
-                    }
-                } else {
-                    source.close();
-                    setResult(JSON.parse(result.slice(11, result.current.length - 9).replace(/&quot;/ig, '"')))
-                }
-            });
-
-            source.addEventListener("readystatechange", (e) => {
-                if (e.readyState >= 2) {
                     setIsLoading(false);
-                }
-            });
+                } catch (error) {
+                    console.log('[DEBUG] error =>>', error)
 
-            source.stream();
+                }
+            }
+            await getData();
         } else {
             alert("Not enough prompt to process! Try again");
         }
@@ -114,6 +95,7 @@ const AIProgess4 = () => {
                                 spacing={1}
                             >
                                 {Object.entries(finalResult).map((key, number) => {
+                                    console.log(Object.entries(finalResult));
                                     return (
                                         <Grid item key={number}>
                                             <p style={{ color: '#000000', fontFamily: 'Inter', fontWeight: 700, fontSize: '32px', textTransform: 'capitalize' }}>
@@ -121,21 +103,27 @@ const AIProgess4 = () => {
                                             </p>
                                             {Object.entries(key[1]).map((option, index) => {
                                                 return (
-                                                    <Box key={index} sx={{ padding: '8px', color: '#000000', fontFamily: 'Inter', fontWeight: 600, fontSize: '16px', textTransform: 'capitalize' }}>
-                                                        <p style={{ fontSize: '18px' }}>Option {index + 1}: {option[1].name}</p>
-                                                        <Box sx={{ display: 'flex', flexDirection: 'column', padding: '8px' }}>
-                                                            <Typography component='div' variant='body1' sx={{ color: '#000000', fontFamily: 'Inter', fontWeight: 300, fontSize: '16px' }}>
-                                                                <b>Pros:</b> {option[1].pros}
-                                                            </Typography>
-                                                            <Typography component='div' variant='body1' sx={{ color: '#000000', fontFamily: 'Inter', fontWeight: 300, fontSize: '16px' }}>
-                                                                <b>Cons:</b> {option[1].cons}
-                                                            </Typography>
+                                                    ((index === 2))
+                                                        ?
+                                                        <p style={{ fontSize: '18px', color: '#000000', fontFamily: 'Inter', fontWeight: 600 }}> {(option[2]?.isOptional) && `This is not require`} </p>
+                                                        :
+                                                        <Box key={index} sx={{ padding: '8px', color: '#000000', fontFamily: 'Inter', fontWeight: 600, fontSize: '16px', textTransform: 'capitalize' }}>
+                                                            <p style={{ fontSize: '18px' }}> Option {index + 1}: {option[1].name}
+
+                                                            </p>
+                                                            <Box sx={{ display: 'flex', flexDirection: 'column', padding: '8px' }}>
+                                                                <Typography component='div' variant='body1' sx={{ color: '#000000', fontFamily: 'Inter', fontWeight: 300, fontSize: '16px' }}>
+                                                                    <b>Pros:</b> {option[1].pros}
+                                                                </Typography>
+                                                                <Typography component='div' variant='body1' sx={{ color: '#000000', fontFamily: 'Inter', fontWeight: 300, fontSize: '16px' }}>
+                                                                    <b>Cons:</b> {option[1].cons}
+                                                                </Typography>
+                                                            </Box>
+                                                            <Stack spacing={4} direction="row" sx={{ marginLeft: '16px' }}>
+                                                                <Button variant='contained' size='small' sx={{ background: '#2DA7FF' }}>See details</Button>
+                                                                <Button variant='contained' size='small' sx={{ background: '#38A169' }}>Buy now</Button>
+                                                            </Stack>
                                                         </Box>
-                                                        <Stack spacing={4} direction="row" sx={{ marginLeft: '16px' }}>
-                                                            <Button variant='contained' size='small' sx={{ background: '#2DA7FF' }}>See details</Button>
-                                                            <Button variant='contained' size='small' sx={{ background: '#38A169' }}>Buy now</Button>
-                                                        </Stack>
-                                                    </Box>
                                                 )
                                             })}
                                         </Grid>
@@ -193,7 +181,6 @@ const AIProgess4 = () => {
                     </Grid>
                 </Grid>
             </Container>
-            <p>{resultRef.current}</p>
             <Footer></Footer>
         </>
     )
